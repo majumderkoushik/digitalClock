d9b5520cd674a67236d3bd31aeaf19d29953df02
majumderkoushik <majumderkoushikme@gmail.com>
2023-09-07 23:21:11 +0530

    pushed

---
+++ script.js
@@ -0,0 +1,24 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minutes");
+const second = document.getElementById("seconds");
+const ampm = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hour.innerText = h;
+  minute.innerText = m;
+  second.innerText = s;
+
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
